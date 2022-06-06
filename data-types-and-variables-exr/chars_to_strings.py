bf64358894b5cba8abeee71a040a7db010547837
Simeon-Fuchedzhiev <sineonfu@gmail.com>
2022-06-06 15:12:10 +0300

    Revert "Completed data types and variables exercises"
    This reverts commit 3b984d9900fc9a45230e722136a136b34bd21919.

--- data-types-and-variables-exr/chars_to_strings.py
+++
@@ -1,6 +0,0 @@
-a = input()
-b = input()
-c = input()
-
-d = a + b + c
-print(d)
--- data-types-and-variables-exr/print_parts_of_ascii_table.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-m = int(input())
-
-for i in range(n,m+1):
-    symbol = chr(i)
-    print(symbol,end=" ")
